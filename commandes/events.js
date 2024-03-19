98d6665106ed2d7cf9d2c86fdd27d1d50e53649b
Oyysreejan <161600696+Oyysreejan@users.noreply.github.com>
2024-03-19 17:46:31 +0530

    Update events.js

--- commandes/events.js
+++ commandes/events.js
@@ -21,7 +21,7 @@ async function events(nomCom) {
                 }
             }
         } else {
-            repondre('You can\'t use this commands ');
+            repondre('Piu-Md is not for every one ');
         }
     });
 }
